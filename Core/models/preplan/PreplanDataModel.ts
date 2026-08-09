03dbf2dac08d9e8b15a76f4ce2df765e792e2992
travellab-ir <pasbanipeyman744@gmail.com>
2026-08-09 14:12:55 +0330

    Add files via upload

--- Core/models/preplan/PreplanDataModel.ts
+++ Core/models/preplan/PreplanDataModel.ts
@@ -3,6 +3,7 @@ import FlightRequirementModel from '@core/models/flight-requirement/FlightRequir
 import FlightModel from '@core/models/flight/FlightModel';
 import PreplanHeaderModel from '@core/models/preplan/PreplanHeaderModel';
 import PreplanVersionModel from '@core/models/preplan/PreplanVersionModel';
+import DisruptionEventModel from '@core/models/preplan/DisruptionEventModel';
 
 export default interface PreplanDataModel {
   readonly header: PreplanHeaderModel;
@@ -11,4 +12,7 @@ export default interface PreplanDataModel {
 
   readonly flightRequirements: readonly FlightRequirementModel[];
   readonly flights: readonly FlightModel[];
+
+  /** Declared operational disruptions (AOG, airport closures, etc.) for this preplan's aircraft registers. */
+  readonly disruptionEvents: readonly DisruptionEventModel[];
 }
